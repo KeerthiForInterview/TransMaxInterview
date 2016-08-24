a0c8a5cf94e3af1d0c3471c92b42110c3d883bdf
Keerthi <kikivenkat82@gmail.com>
2016-08-24 12:34:20 +1000

    TransMax test completed with unit test project.

--- TransmaxTest/Program.cs
+++ TransmaxTest/Program.cs
@@ -1,15 +1,137 @@
-﻿using System;
+﻿using LINQtoCSV;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TransmaxTest
+namespace Transmax
 {
-    class Program
+    public class Student
+    {
+        public Student()
+        {
+            InstantiateCSVObjects();
+        }
+
+        public IEnumerable<Student> ReadFile(string fileName)
+        {
+            return _csvContext.Read<Student>(fileName, _csvFileDescription);
+        }
+
+        public IEnumerable<Student> SortByMarksAndLastName(IEnumerable<Student> students)
+        {
+            return students.OrderByDescending(s => s.Marks).ThenBy(s => s.LastName);
+        }
+        public void WriteFile(string fileName, IEnumerable<Student> students)
+        {
+            _csvContext.Write<Student>(students.ToList(), fileName, _csvFileDescription);
+        }
+
+        public void PrintContents(string fileName)
+        {
+            var fileContents = File.ReadAllLines(fileName);
+            foreach (var line in fileContents)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        public string GetOuputFileName(string inputFileName)
+        {
+            return string.Concat(System.IO.Path.GetFileNameWithoutExtension(inputFileName), "-graded.txt");
+        }
+
+        private void InstantiateCSVObjects()
+        {
+            if (_csvContext == null)
+            {
+                _csvContext = new CsvContext();
+            }
+            if (_csvFileDescription == null)
+            {
+                _csvFileDescription = new CsvFileDescription()
+                {
+                    SeparatorChar = ',',
+                    FirstLineHasColumnNames = false,
+                    EnforceCsvColumnAttribute = true,
+                    MaximumNbrExceptions = 1
+
+                };
+            }
+        }
+
+        [CsvColumn(FieldIndex = 1)]
+        public string LastName { get; set; }
+
+        [CsvColumn(FieldIndex = 2)]
+        public string FirstName { get; set; }
+
+        [CsvColumn(FieldIndex = 3)]
+        public int Marks { get; set; }
+
+        private static CsvContext _csvContext;
+        private static CsvFileDescription _csvFileDescription;
+    };
+
+
+    public class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                System.Console.WriteLine("Incorrect usage! Please give the filename to be processed. eg- TransmaxTest c:\\FileToBeProcessed.txt");
+                return;
+            }
+            else
+            {
+                string inputFileName = args[0];
+                try
+                {
+                    var student = new Student();
+                    IEnumerable<Student> students = null;
+
+                    try
+                    {
+                        students = student.ReadFile(inputFileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(String.Format("Could not read from file: {0}", ex.Message));
+                        throw ex;
+                    }
+
+                    if (students.Count() > 0)
+                    {
+                        var studentsSortByMarks = student.SortByMarksAndLastName(students);
+
+                        string outputFileName = student.GetOuputFileName(inputFileName);
+
+                        try
+                        {
+                            student.WriteFile(outputFileName, studentsSortByMarks);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(String.Format("Could not write into file: {0}", ex.Message));
+                            throw ex;
+                        }
+
+                        student.PrintContents(outputFileName);
+                        Console.WriteLine(String.Format("Finished: Created {0}", outputFileName));
+                    }
+                    else
+                    {
+                        Console.WriteLine("The file is empty!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+
+                }
+
+            }
         }
     }
 }
---
+++ UnitTestTransMax/UnitTest1.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Transmax;
+
+namespace TransMaxUnitTests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        private int StartConsoleApplication(string arguments)
+        {
+            // Initialize process here
+            Process proc = new Process();
+            proc.StartInfo.FileName = @"..\..\..\TransmaxTest\bin\Release\TransmaxTest.exe";
+            // add arguments as whole string
+            proc.StartInfo.Arguments = arguments;
+
+            // use it to start from testing environment
+            proc.StartInfo.UseShellExecute = false;
+
+            // redirect outputs to have it in testing console
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+
+            // set working directory
+            proc.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+
+            // start and wait for exit
+            proc.Start();
+            proc.WaitForExit();
+
+            // get output to testing console.
+            Console.WriteLine(proc.StandardOutput.ReadToEnd());
+            Console.Write(proc.StandardError.ReadToEnd());
+
+            // return exit code
+            return proc.ExitCode;
+        }
+
+        [TestMethod]
+        public void ValidateUsage()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                StartConsoleApplication(null);
+                Assert.IsTrue(sw.ToString().Contains("Incorrect usage"));
+            }
+        }
+
+        [TestMethod]
+        public void ValidateInputFile()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                StartConsoleApplication("ThisFileDoesNotExist.txt");
+                Assert.IsTrue(sw.ToString().Contains("Could not find file"));
+            }
+        }
+
+        [TestMethod]
+        public void ValidateEmptyFile()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                string inputFileName = @"InputFiles\InputFileEmpty.txt";
+                StartConsoleApplication(inputFileName);
+
+                Assert.IsTrue(sw.ToString().Contains("The file is empty"));
+            }
+        }
+
+        [TestMethod]
+        public void ValidateStudentCollectionIfMarkAbsent()
+        {
+            var testStudent = new Student();
+            string inputFileName = @"InputFiles\MarksMissingInput.txt";
+            var studentsList = testStudent.ReadFile(inputFileName);
+
+            IEnumerable<Student> sortedStudentsList = testStudent.SortByMarksAndLastName(studentsList);
+
+            string outputFileName = @"ActualResult\" + testStudent.GetOuputFileName(inputFileName);
+            testStudent.WriteFile(outputFileName, sortedStudentsList);
+
+            string expectedResultFileName = @"ExpectedResult\MarksMissingExpected.txt";
+            IEnumerable<Student> expectedSortedStudentsList = testStudent.ReadFile(expectedResultFileName);
+
+            AssertCollectionsAreEqual(expectedSortedStudentsList, sortedStudentsList);
+
+        }
+
+        [TestMethod]
+        public void ValidateStudentCollectionIfLastNamekAbsent()
+        {
+            var testStudent = new Student();
+            string inputFileName = @"InputFiles\LastNameMissingInput.txt";
+
+            var studentsList = testStudent.ReadFile(inputFileName);
+
+            IEnumerable<Student> sortedStudentsList = testStudent.SortByMarksAndLastName(studentsList);
+
+            string outputFileName = @"ActualResult\" + testStudent.GetOuputFileName(inputFileName);
+
+            testStudent.WriteFile(outputFileName, sortedStudentsList);
+
+            string expectedResultFileName = @"ExpectedResult\LastNameMissingExpected.txt";
+            IEnumerable<Student> expectedSortedStudentsList = testStudent.ReadFile(expectedResultFileName);
+
+            AssertCollectionsAreEqual(expectedSortedStudentsList, sortedStudentsList);
+        }
+
+        [TestMethod]
+        public void ValidateStudentCollectionIfFirstNamekAbsent()
+        {
+            var testStudent = new Student();
+            string inputFileName = @"InputFiles\FirstNameMissingInput.txt";
+
+            var studentsList = testStudent.ReadFile(inputFileName);
+
+            IEnumerable<Student> sortedStudentsList = testStudent.SortByMarksAndLastName(studentsList);
+
+            string outputFileName = @"ActualResult\" + testStudent.GetOuputFileName(inputFileName);
+
+            testStudent.WriteFile(outputFileName, sortedStudentsList);
+
+            string expectedResultFileName = @"ExpectedResult\FirstNameMissingExpected.txt";
+            IEnumerable<Student> expectedSortedStudentsList = testStudent.ReadFile(expectedResultFileName);
+
+            AssertCollectionsAreEqual(expectedSortedStudentsList, sortedStudentsList);
+        }
+
+        [TestMethod]
+        public void TestForGoodData()
+        {
+            var testStudent = new Student();
+            string inputFileName = @"InputFiles\GoodDataInput.txt";
+
+            var studentsList = testStudent.ReadFile(inputFileName);
+
+            IEnumerable<Student> sortedStudentsList = testStudent.SortByMarksAndLastName(studentsList);
+
+            string outputFileName = @"ActualResult\" + testStudent.GetOuputFileName(inputFileName);
+
+            testStudent.WriteFile(outputFileName, sortedStudentsList);
+
+            string expectedResultFileName = @"ExpectedResult\GoodDataExpected.txt";
+            IEnumerable<Student> expectedSortedStudentsList = testStudent.ReadFile(expectedResultFileName);
+
+            AssertCollectionsAreEqual(expectedSortedStudentsList, sortedStudentsList);
+        }
+
+        private void AssertCollectionsAreEqual(IEnumerable<Student> expected, IEnumerable<Student> actual)
+        {
+            int count = expected.Count();
+
+            Assert.AreEqual(count, actual.Count());
+
+            for (int i = 0; i < count; i++)
+            {
+                Assert.AreEqual(expected.ElementAt(i).FirstName, actual.ElementAt(i).FirstName);
+                Assert.AreEqual(expected.ElementAt(i).LastName, actual.ElementAt(i).LastName);
+                Assert.AreEqual(expected.ElementAt(i).Marks, actual.ElementAt(i).Marks);
+            }
+        }
+    }
+}
